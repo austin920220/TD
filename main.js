6667d3af20ab64d35937fc67d63436cc80ea13ba
austin920220 <austin.98007@gmail.com>
2016-01-09 12:15:22 +0800

    Update main.js

--- main.js
+++ main.js
@@ -14,16 +14,6 @@ move: function(){
 	this.y = this.y + this.direction.y * this.speed/FPS;
 }
 };
-function getUnitVector (srcX, srcY, targetX, targetY) {
-    var offsetX = targetX - srcX;
-    var offsetY = targetY - srcY;
-    var distance = Math.sqrt( Math.pow(offsetX,2) + Math.pow(offsetY,2) );
-    var unitVector = {
-        x: offsetX/distance,
-        y: offsetY/distance
-    };
-    return unitVector;
-}
 
 var enemyPath = [
 	{x:96, y:64},
@@ -95,4 +85,13 @@ function isCollided(pointX, pointY, targetX, targetY, targetWidth, targetHeight)
 		return false;
 	}
 }
-
+function getUnitVector (srcX, srcY, targetX, targetY) {
+    var offsetX = targetX - srcX;
+    var offsetY = targetY - srcY;
+    var distance = Math.sqrt( Math.pow(offsetX,2) + Math.pow(offsetY,2) );
+    var unitVector = {
+        x: offsetX/distance,
+        y: offsetY/distance
+    };
+    return unitVector;
+}
