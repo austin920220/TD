5b2222a9cf02293fcdda38026b999c4601b07ddf
austin920220 <austin.98007@gmail.com>
2016-01-09 13:11:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,8 +10,23 @@ y:480-32,
 direction:{x:0,y:-1},
 speed:32,
 move: function(){
-	this.x = this.x + this.direction.x * this.speed/FPS;
-	this.y = this.y + this.direction.y * this.speed/FPS;
+	if(isCollided(enemyPath[this.pathDes].x, enemyPath[this.pathDes].y, this.x, this,y, this.speed/FPS, this.speed/FPS)){
+		// 首先，移動到下一個路徑點
+            this.x = enemyPath[this.pathDes].x;
+            this.y = enemyPath[this.pathDes].y;
+
+            // 指定下一個路徑點
+            this.pathDes++;
+
+            // 取得前往下一個路徑點的單位向量
+            var unitVector = getUnitVector( this.x, this.y, enemyPath[this.pathDes].x, enemyPath[this.pathDes].y );
+            this.direction.x = unitVector.x;
+            this.direction.y = unitVector.y;
+		
+	} else {
+		this.x = this.x + this.direction.x * this.speed/FPS;
+		this.y = this.y + this.direction.y * this.speed/FPS;
+	}
 }
 };
 
