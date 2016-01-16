79453e90c24ee8e75f47513875e9f4abec55117f
austin920220 <austin.98007@gmail.com>
2016-01-16 11:27:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var FPS = 60;
 var cursor = {};
 var isBuilding = false;
 var tower = {};
-var enemies = new Enemy();
+var enemy = new Enemy();
 function Enemy() { 
     this.x = 96; 
     this.y = 480-32;
